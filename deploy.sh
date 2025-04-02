0845934200e2f44e99aff7ee595a461bb9776710
vinobc <vinobchanderr@gmail.com>
2025-04-02 05:56:21 +0000

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cd /var/www/scfc-erp
-git pull
+git fetch
+git checkout production-fixes  # Always use this branch in production
+git pull origin production-fixes
 npm install
 pm2 restart scfc-erp
